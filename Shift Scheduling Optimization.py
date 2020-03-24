204a404d0c87bf950d1be2e4891d0f3d89b9eda0
Baski Balasundaram <baski.b@protonmail.com>
2020-03-24 16:12:39 -0500

    Update Shift Scheduling Optimization.py
    Simplified parameters being read. Only the data read fro mfile now. Model solves to feasibility when delta=0.

--- Shift Scheduling Optimization.py
+++ Shift Scheduling Optimization.py
@@ -10,33 +10,33 @@ import pandas as pd
 
 #Importing Data
 datafile = "demand_1.csv"
-file = "IP Project2 Data.TXT"
-X=np.loadtxt(file, skiprows=2).astype(int)
 R = pd.read_csv(datafile,header=None)
 
 #Parameters:
 #minimum and maximum duration of shifts
-d_min = X[0,0]
-d_max=X[0,1]
-#number of shifts, number of schedules
-n=X[1,0]
-m=X[1,1]
-print(n)
+d_min = 32
+d_max = 40
+#number of shifts
+n = 10
+# number of schedules
+m = 30
 #number of time slots 
-T = X[2,0]
-#Getting Time limit in seconds
-tl= X[3,0]
+T = 1344
 #break between the shifts
-delta= X[4,0]
+delta= 32
 #Requirements of every time slot
 r = np.array(R)
+#Getting Time limit in seconds
+tl= 30
+
+
 #Defining the model
 modell = gp.Model("Shift Scheduling Optimization")
 
 # Decision variables
 startt = modell.addVars(range(1,n+1),range(1,m+1), vtype=GRB.INTEGER, obj=-1)
 complt = modell.addVars(range(1,n+1),range(1,m+1), vtype=GRB.INTEGER, obj=1)
-z_ijk = modell.addVars(range(1,n+1),range(1,m+1),range(1,T+1), vtype=GRB.BINARY)
+cover_ind = modell.addVars(range(1,n+1),range(1,m+1),range(1,T+1), vtype=GRB.BINARY)
 
 # We want to minimize the total costs
 modell.modelSense = GRB.MINIMIZE
@@ -45,9 +45,9 @@ modell.modelSense = GRB.MINIMIZE
 modell.addConstrs(startt[i+1,j]>=complt[i,j]+delta for i in range(1,n) for j in range(1,m+1))
 modell.addConstrs(complt[i,j]-startt[i,j]>=d_min for i in range(1,n+1) for j in range(1,m+1))
 modell.addConstrs(complt[i,j]-startt[i,j]<=d_max for i in range(1,n+1) for j in range(1,m+1))
-modell.addConstrs(sum(z_ijk[i,j,k] for i in range(1,n+1) for j in range(1,m+1))>=r[k-1][0] for k in range(1,T+1))
-modell.addConstrs(startt[i,j]<=k*z_ijk[i,j,k]+T*(1-z_ijk[i,j,k]) for i in range(1,n+1) for j in range(1,m+1) for k in range(1,T+1))
-modell.addConstrs(complt[i,j]>=k*z_ijk[i,j,k] for i in range (1,n+1) for j in range(1,m+1) for k in range(1,T+1))
+modell.addConstrs(sum(cover_ind[i,j,k] for i in range(1,n+1) for j in range(1,m+1))>=r[k-1][0] for k in range(1,T+1))
+modell.addConstrs(startt[i,j]<=k*cover_ind[i,j,k]+T*(1-cover_ind[i,j,k]) for i in range(1,n+1) for j in range(1,m+1) for k in range(1,T+1))
+modell.addConstrs(complt[i,j]>=k*cover_ind[i,j,k] for i in range (1,n+1) for j in range(1,m+1) for k in range(1,T+1))
 
 #Time limit
 #modell.params.timelimit= tl
